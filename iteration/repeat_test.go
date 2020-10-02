1d44b6721742771746b6a7cf810128dda1688dd9
Ameris <rudlandameris@gmail.com>
2020-10-02 16:20:32 -0400

    added strings pkg functions and refactored test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,17 +2,41 @@ package iteration
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	response := Repeat("a", 5)
-	expected := "aaaaa"
-
-	if response != expected {
-		t.Errorf("expected %q but got %q", expected, response)
+	testWithParams := func(t *testing.T, char string, repeatNum int) (string, string) {
+		t.Helper()
+		response := Repeat(char, repeatNum)
+		expected := strings.Repeat(char, repeatNum)
+		return response, expected
 	}
+
+	t.Run("test with EqualFold", func(t *testing.T) {
+		response, expected := testWithParams(t, "a", 4)
+		if !strings.EqualFold(response, expected) {
+			t.Errorf("expected %q but got %q", expected, response)
+		}
+	})
+
+	t.Run("test with Compare", func(t *testing.T) {
+		response, expected := testWithParams(t, "b", 5)
+		if strings.Compare(response, expected) != 0 {
+			t.Errorf("expected %q but got %q", expected, response)
+		}
+	})
+
+	t.Run("test with Count", func(t *testing.T) {
+		char := "c"
+		response, expected := testWithParams(t, char, 6)
+		if strings.Count(response, char) != strings.Count(expected, char) {
+			t.Errorf("expected %d chars but got %d chars", strings.Count(expected, char), strings.Count(response, char))
+		}
+	})
 }
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
