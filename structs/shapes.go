dc795693043e888ae93492eadba4edd1f7f7600a
Ameris <rudlandameris@gmail.com>
2021-01-11 11:19:19 -0500

    done shapes

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,7 +1,21 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
 type Rectangle struct {
 	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
 	Height float64
 }
 
@@ -9,6 +23,14 @@ func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Height + r.Width)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -7,15 +7,24 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("expected %.2f but got %.2f", got, want)
+		t.Errorf("expected %.2f but got %.2f", want, got)
 	}
 }
 
 func TestArea(t *testing.T) {
-	got := Area(Rectangle{10.0, 10.0})
-	want := 100.0
+	areaTests := []struct {
+		shape Shape
+		hasArea  float64
+	}{
+		{shape: Rectangle{Width:12, Height: 6}, hasArea: 72.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
 
-	if got != want {
-		t.Errorf("expected %.2f but got %.2f", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v expected %.2f but got %.2f", tt.shape, tt.hasArea, got)
+		}
 	}
 }
